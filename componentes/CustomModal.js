67836f834c69addb920052eca798be3d0e8c8fb8
Víctor M <victormunozfer@gmail.com>
2022-11-08 19:58:49 -0300

    Cambios

--- componentes/CustomModal.js
+++ componentes/CustomModal.js
@@ -7,29 +7,30 @@ const CustomModal = () => {
  const [modalVisible, setModalVisible] = useState(false);// Se crea un booleano modalvisible para ir actualizando el estado del modal con el hook useState cada vez que apretamos el boton
   return (
     <View style={styles.centeredView}> 
+
       <Modal
         animationType="slide" //Prop que controla la animacion del modal slide, fade o none
         transparent={true} //Prop que si esta true entrega un fondo transparente al abrir el modal y false un fondo blanco.
         visible={modalVisible} //Prop que determina cuando el modal esta visible
       >
-        
+        <View style={styles.centeredView}>
         <View style={styles.modalView}> 
           <Image style={styles.image} source={Warning}/>
-            <Text style={styles.modalText}>Este es un modal</Text>
+            <Text style={styles.titleModal}>Este es un modal</Text>
           
             <Pressable
-              style={[styles.button, styles.buttonClose]}
+              style={[styles.button]}
               onPress={() => setModalVisible(!modalVisible)}
             >
               <Text style={styles.textStyle}>ESCONDER MODAL</Text>
             </Pressable>
 
           </View>
-
+        </View>
       </Modal>
       
       <Pressable
-        style={[styles.button, styles.buttonOpen]}
+        style={[styles.button]}
         onPress={() => setModalVisible(true)}
       >
         <Text style={styles.textStyle}>ABRIR MODAL</Text>
@@ -42,50 +43,39 @@ const CustomModal = () => {
 };
 
 const styles = StyleSheet.create({
+  
   image:{
-    width:'100%'
+    width:50,
+    height:50
   },
   centeredView: {
-    flex: 1,
+    flex:1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
   },
   modalView: {
-    margin: 20,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 35,
+    padding:20,
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
+    shadowOpacity: 0.5,
   },
   button: {
-    borderRadius: 20,
+    marginTop:10,
     padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
+    backgroundColor:'black'
   },
   textStyle: {
     color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    fontWeight:"bold"
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
+  titleModal:{
+    fontSize:20,
+    marginTop:10,
+    fontWeight:'bold'
+  },
+  
 });
 
 export default CustomModal;
